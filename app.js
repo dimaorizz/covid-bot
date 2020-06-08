b196296ac08dd8fbade17643cde0e2fe831145e0
Orizz <dimaorizz@gmail.com>
2020-06-08 18:59:25 +0300

    ukraine stats added

--- app.js
+++ app.js
@@ -22,8 +22,25 @@ bot.command('world', async ctx => {
 <b>Новых случаев сегодня:</b> <i>${worldStats.total_new_cases_today.toLocaleString()}</i>
 <b>Смертей сегодня:</b> <i>${worldStats.total_new_deaths_today.toLocaleString()}</i>`;
 
-    console.log(worldStats);
     await ctx.reply(message, { parse_mode: 'HTML'});
-})
+});
+
+bot.command('ukraine', async ctx => {
+    const countryID = 'UA';
+    let ukraineStats = await fetch(`https://api.thevirustracker.com/free-api?countryTotal=${countryID}`);
+    ukraineStats = await ukraineStats.json();
+    ukraineStats = ukraineStats.countrydata[0];
+
+    const message = 
+    `<b>Статистика по Украине</b>\n
+<b>Всего случаев заражения COVID:</b> <i>${ukraineStats.total_cases.toLocaleString()}</i>
+<b>Выздоровело:</b> <i>${ukraineStats.total_recovered.toLocaleString()}</i>
+<b>Болеют:</b> <i>${ukraineStats.total_unresolved.toLocaleString()}</i>
+<b>Умерло:</b> <i>${ukraineStats.total_deaths.toLocaleString()}</i>
+<b>Новых случаев сегодня:</b> <i>${ukraineStats.total_new_cases_today.toLocaleString()}</i>
+<b>Смертей сегодня:</b> <i>${ukraineStats.total_new_deaths_today.toLocaleString()}</i>`;
+
+    await ctx.reply(message, { parse_mode: 'HTML'});
+});
 
 bot.launch();
